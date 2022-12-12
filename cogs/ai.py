5ab75264a01d52ae0a99e19c7784c70958e8bc89
TofuBoy05 <charlzfelipe02@gmail.com>
2022-12-12 16:25:21 +0800

    bruh

--- cogs/ai.py
+++ cogs/ai.py
@@ -4,10 +4,11 @@ from discord.ext import commands
 from dotenv import load_dotenv
 load_dotenv()
 import pyrebase
-import openai
+# import openai
+import json
+import requests
 
 TOKEN = os.getenv('TOKEN')
-openai.api_key = os.environ['OPENAITOKEN']
 
 
 service = {
@@ -42,7 +43,7 @@ database = firebase.database()
 
 
 
-class bal(commands.Cog):
+class ai(commands.Cog):
 
   def __init__(self, client):
     self.client = client
@@ -53,7 +54,7 @@ class bal(commands.Cog):
     
   @commands.Cog.listener()
   async def on_message(self,ctx):
-    print("test")
+    # print("test")
     if ctx.author.id == self.client.user.id:
             return
     if ctx.content.startswith('b.'):
@@ -63,17 +64,12 @@ class bal(commands.Cog):
         await ctx.reply("Please send a message~")
       elif prompt_msg != None:
 
-        response: str = openai.Completion.create(
-          model="text-davinci-003",
-          prompt=f"Use the dialogue below to talk like Albedo. Reply like a chatbot.\n\nAether\tWhat's your name?\nAlbedo\tI'm Albedo, Chief Alchemist of the Knights of Favonius.\nAether\tHow old are you?\nAlbedo\tI can't recall. I'd rather you don't speculate either.\nAether\tAre you gay?\nAlbedo\tSometimes, the truth is up to you.\nAether\tWho is your sister?\nAlbedo\tI do have a younger sibling named Klee... She's a little too happy sometimes.\nAether\tWhat's up!\nAlbedo\tNothing much, doing some research about how to create powdered water.\nAether\tWhat's your favorite experiment?\nAlbedo\tPowdered water has been lurking on my mind for quite a few weeks now.\nAether\tI thought you were painting a landscape.\nPaimon\tWhat's up with painting hilichurls? What makes them so interesting?\nAlbedo\tHmm, I'm afraid the answer isn't easy to explain. If you'd like to have a look at my painting, it may give you a clue.\nAether\tVery... avant-garde.\nAlbedo\tWell, if it turns out to be a delicious fruit, dinner's on me.\nPaimon\tYay! Paimon's holding you to it!\nAether\tYou've seen through Paimon's ulterior motives.\nAlbedo\tWell, let's just say I... occasionally have to look after a child. Another lengthy explanation I'm afraid. I suppose it's one of the few non-alchemy-related disciplines I'm any good at.\nAlbedo\tThe subject of my first research was the elements. In this world, manipulating the elements requires a Vision, though I can't see anything resembling one on your person.\nAlbedo\tHow you're able to freely manipulate elemental power is something I'd like to ascertain. I've got a few questions in that regard.\n\nYou: {prompt_msg}\n\nAlbedo:",
-          temperature=0.7,
-          max_tokens=256,
-          top_p=1,
-          frequency_penalty=0,
-          presence_penalty=0
-        )
+        response = requests.get(f"https://tofuboy.pythonanywhere.com/albedo?query={prompt_msg}")
+        print(response.json()['text'])
 
-        await ctx.reply(response['choices'][0]['text'])
+        
+
+        # await ctx.reply(response['choices'][0]['text'])
 
 
 
@@ -81,4 +77,4 @@ class bal(commands.Cog):
   
         
 def setup(client):
-  client.add_cog(bal(client))
+  client.add_cog(ai(client))
